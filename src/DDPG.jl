4845df8d96e882328963224182948d99a8848e2c
Dehaybe <henri.dehaybe@uclouvain.be>
2020-11-09 11:20:00 +0100

    cancel merge

--- src/DDPG.jl
+++ src/DDPG.jl
@@ -37,28 +37,17 @@ end
     softsync_rate::Float64 = 0.001
     discount::Float32 = 0.99f0
 	optimiser_critic = ADAM(1f-4)
-    optimiser_actor = Flux.Optimiser(Flux.ClipNorm(1f-6), ADAM(1.25f-5))
-end
-
-@with_kw struct TD3_HP
-	replaysize::Int = 2^15
-	batchsize::Int = 128
-    softsync_rate::Float64 = 0.001
-    discount::Float32 = 0.99f0
-	optimiser_critic = ADAM(1f-4)
-    optimiser_actor = Flux.Optimiser(Flux.ClipNorm(1f-6), ADAM(1.25f-5))
+	optimiser_actor = Flux.Optimiser(Flux.ClipNorm(1f-6), ADAM(1.25f-5))
 end
 
 Flux.gpu(a::DDPGQN_Agent) = DDPGQN_Agent(a.actor |> gpu, a.critic |> gpu, a.explorer) 
 Flux.cpu(a::DDPGQN_Agent) = DDPGQN_Agent(a.actor |> cpu, a.critic |> cpu, a.explorer)
 
-function train!(agent, envi, hyperparameters::TD3_HP; maxit::Int = 500000, test_freq::Int = maxit, verbose = false, progress_bar = false, dynamic_envis = [])
+function train!(agent, envi, hyperparameters::DDPG_HP; maxit::Int = 500000, test_freq::Int = maxit, verbose = false, progress_bar = false, dynamic_envis = [])
     starttime = Base.time()
-    @unpack replaysize, batchsize, softsync_rate, discount, optimiser_actor, optimiser_critic, td3 = hyperparameters
-    target_agent = deepcopy(agent)
+    @unpack replaysize, batchsize, softsync_rate, discount, optimiser_actor, optimiser_critic = hyperparameters
+	target_agent = deepcopy(agent)
     
-    twin = deepcopy(agent.critic)
-    target_twin = deepcopy(twin)
     
     test_envi = deepcopy(envi)
     test_reset!(test_envi)
@@ -70,25 +59,15 @@ function train!(agent, envi, hyperparameters::TD3_HP; maxit::Int = 500000, test_
 	progress = Progress(maxit)
 	for it = 1:maxit
         s,a,r,ns,d = ksample(replaybuffer)
-        y = target(r,ns,d,discount, target_agent,target_twin)
+        y = target(r,ns,d,discount, target_agent)
         b = (s,a,y)
-        if it%2 == 1
-            Flux.train!(
-                (d...) -> critic_loss(d..., agent.critic),
-                Flux.params(agent.critic),
-                [b],
-                optimiser_critic
-            )
-            softsync!(agent.critic, target_agent.critic, softsync_rate)
-        else
-            Flux.train!(
-                (d...) -> critic_loss(d..., twin),
-                Flux.params(twin),
-                [b],
-                optimiser_critic
-            )
-            softsync!(twin, target_twin, softsync_rate)
-        end
+        Flux.train!(
+            (d...) -> critic_loss(d..., agent.critic),
+            Flux.params(agent.critic),
+            [b],
+            optimiser_critic
+        )
+        softsync!(agent.critic, target_agent.critic, softsync_rate)
         s,a,r,ns,d = ksample(replaybuffer)
         Flux.train!(
             s -> actor_loss(s, agent),
@@ -117,14 +96,6 @@ function target(reward::T, next_state::T, done::T, discount::Float32, target_age
     next_action = target_agent(next_state)
     Qprime = target_agent.critic(vcat(next_state, next_action)) #.* done
     return reward .+ Qprime .* discount
-end
-
-function target(reward::T, next_state::T, done::T, discount::Float32, target_agent, twin) where T <: AbstractArray
-    next_action = target_agent(next_state)
-    sa = vcat(next_state, next_action)
-    Qprime = target_agent.critic(sa)
-    Qprimetwin = twin(sa)
-    return reward .+ min.(Qprime, Qprimetwin) .* discount
 end
 
 function critic_loss(state::T, action::T, y::T, critic) where T <: AbstractArray
